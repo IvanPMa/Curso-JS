5c5588373be4458005fe8e48b42a370d343b9c25
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-16 11:17:21 -0600

    Ejercicio 2

---
+++ Ejercicio 2/ejercicio2.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var nombre1 = 'Pablo';
+var calificacionesPablo = [14,8,16];
+var nombre2 = 'Maria';
+var calificacionesMaria = [12,18,10];
+
+var promedioPablo = calcularPromedio(calificacionesPablo);
+console.log("Promedio Pablo: "+promedioPablo+" : "+aprobatorio(promedioPablo));
+
+var promedioMaria = calcularPromedio(calificacionesMaria);
+console.log("Promedio Maria: "+ promedioMaria+ " : "+aprobatorio(promedioMaria));
+
+mayor(promedioMaria,promedioPablo);
+
+
+
+function  calcularPromedio(elemento){
+    var suma = 0, contador=0,promedio; 
+    for (var i in elemento){
+         suma += elemento[i];
+         contador++;
+    }
+    return promedio = suma/contador;
+}
+
+function aprobatorio(valor){
+    var situacion;
+    valor > 13 ? situacion ="Aprobado" : situacion= "reprobado"
+    return  situacion;
+}
+
+function mayor(promedio1,promedio2){
+    promedio1 > promedio2 ? console.log('Maria tiene mayor promedio') :('Pablo tiene mayor promedio');
+}
