0931ab08844ea493e6a576b19150ab876798b9f9
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-16 08:22:18 -0600

    Variables

---
+++ 01-Fundamentos/js/app.js
@@ -0,0 +1,17 @@
+var primerNombre = 'Ivan';
+console.log(primerNombre);
+
+var edad = 22;
+var sueldo = 1800.99;
+
+console.log(edad);
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+
+
+var puestoDeTrabajo;
+console.log(puestoDeTrabajo);
+
+tieneTrabajo= null;
+console.log(tieneTrabajo);
